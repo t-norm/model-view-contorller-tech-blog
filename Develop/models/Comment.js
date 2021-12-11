d1357dc4c92d78b6067341e14f119b572bba136c
Tyler Norman <tnorman600@gmail.com>
2021-12-11 11:32:52 -0700

    updated models

--- Develop/models/Comment.js
+++ Develop/models/Comment.js
@@ -1,8 +1,34 @@
-const { Model } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Comment extends Model {};
 
-Comment.init({},{sequelize});
+Comment.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'post', key: 'id' }
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: 'user', key: 'id' }
+    }},
+    {
+        sequelize,
+        modelName: 'comment',
+        freezeTableName: true,
+        underscored: true
+});
 
 module.exports = Comment;
